c453626d6b84894f2233f77003f0764aac4e7f62
GerarPutraN <86067293+GerarPutra@users.noreply.github.com>
2026-07-06 13:30:20 +0700

    Implement GUI for echo effect parameter tuning
    Added a GUI for parameter tuning of delay time and feedback gain in the echo effect.

--- echo_sysobj.m
+++ echo_sysobj.m
@@ -1,7 +1,7 @@
 clear; clc; close all;
 
 frameLength = 1024;
-inputFile = 'y2mate.mp3';
+inputFile = 'oasis-wonderwall-official-video-0-mhqin.wav';
 
 fileReader = dsp.AudioFileReader(inputFile, ...
     'SamplesPerFrame', frameLength, 'PlayCount', 1);
@@ -26,6 +26,56 @@ scope = timescope( ...
     'ShowLegend', true, ...
     'Title', 'Echo Effect (Delay + Feedback)', ...
     'ChannelNames', {'Echo Output', 'Original'});
+fig = figure('Name', 'Parameter Tuner', 'Position', [100, 100, 350, 320], ...
+    'Color', [0.94, 0.94, 0.94]);
+
+paramList = {
+    'delayTimeSec', 0.4, 0, 1;
+    'feedbackGain', 0.5, 0, 1
+    };
+
+yPos = 270;
+for i = 1:size(paramList, 1)
+    name = paramList{i, 1};
+    defaultVal = paramList{i, 2};
+    minVal = paramList{i, 3};
+    maxVal = paramList{i, 4};
+
+    uicontrol('Style', 'text', 'String', name, ...
+        'Position', [10, yPos, 100, 20], 'BackgroundColor', [0.94, 0.94, 0.94]);
+
+    hSlider = uicontrol('Style', 'slider', 'Min', minVal, 'Max', maxVal, ...
+        'Value', defaultVal, 'Position', [110, yPos+2, 150, 16]);
+
+    hEdit = uicontrol('Style', 'edit', 'String', sprintf('%.3f', defaultVal), ...
+        'Position', [270, yPos, 60, 20]);
+
+    set(hSlider, 'Callback', @(src,~) onSliderChange(src, hEdit, name));
+    set(hEdit, 'Callback', @(src,~) onEditChange(src, hSlider, name));
+
+    yPos = yPos - 40;
+end
+
+function onSliderChange(hSlider, hEdit, paramName)
+val = get(hSlider, 'Value');
+set(hEdit, 'String', sprintf('%.4f', val));
+applyParameter(paramName, val);
+end
+
+function onEditChange(hEdit, hSlider, paramName)
+val = str2double(get(hEdit, 'String'));
+if ~isnan(val)
+    set(hSlider, 'Value', val);
+    applyParameter(paramName, val);
+    end
+end
+
+function applyParameter(name, value)
+switch name
+    case 'delayTimeSec'
+        delayObj.delayTimeSec = value;
+    end
+end 
 
 while ~isDone(fileReader)
     audioIn = fileReader();
